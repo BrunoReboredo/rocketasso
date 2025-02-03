58cd16035186efbcb4d4114d1ec0f8bd30fe541b
BrunoReboredo <brurebo@gmail.com>
2025-02-03 20:10:11 +0100

    Implementado el menu despegable de competicion

--- web-rck/src/Navbar.tsx
+++ web-rck/src/Navbar.tsx
@@ -1,8 +1,15 @@
 import './Navbar.css';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from './assets/RCK_Logo_Cua_RenderMor.png'; // Asegúrate de que la ruta es correcta
 
 const Navbar = () => {
+
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const handleDropdownToggle = () => {
+    setIsDropdownOpen(!isDropdownOpen); // Alterna el estado del dropdown
+  };
   return (
     <nav className="navbar">
       <div className="navbar-logo">
@@ -11,8 +18,21 @@ const Navbar = () => {
         </Link>
       </div>
       <ul className="navbar-links">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/competition">Competición</Link></li>
+      <li><Link to="/">Home</Link></li>
+        <li
+          className="dropdown"
+          onMouseEnter={handleDropdownToggle}
+          onMouseLeave={handleDropdownToggle}
+        >
+          <Link to="/competition">Competición</Link>
+          {isDropdownOpen && (
+            <ul className="dropdown-menu">
+              <li><Link to="/competition/season1">Season1</Link></li>
+              <li><Link to="/competition/season2">Season2</Link></li>
+              <li><Link to="/competition/rules">Reglamento</Link></li>
+            </ul>
+       )}
+        </li>
         <li><Link to="/news">Noticias</Link></li>
         <li><Link to="/meet-us">Conócenos</Link></li>
         <li><Link to="/contact">Contacta con nosotros</Link></li>
