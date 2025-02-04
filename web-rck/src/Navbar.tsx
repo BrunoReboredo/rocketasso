03185b34c484bf6c1149240fd2e669ea668cc5ea
BrunoReboredo <brurebo@gmail.com>
2025-02-04 14:15:30 +0100

    acutralizacion en el apartado de competicion

--- web-rck/src/Navbar.tsx
+++ web-rck/src/Navbar.tsx
@@ -1,15 +1,22 @@
 import './Navbar.css';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import logo from './assets/RCK_Logo_Cua_RenderMor.png'; // Asegúrate de que la ruta es correcta
+import logo from './assets/RCK_Logo_Cua_RenderNeg.png'; // Asegúrate de que la ruta es correcta
 
 const Navbar = () => {
 
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(false); // Estado para el submenú
+
 
   const handleDropdownToggle = () => {
     setIsDropdownOpen(!isDropdownOpen); // Alterna el estado del dropdown
   };
+
+  const handleSubmenuToggle = () => {
+    setIsSubmenuOpen(!isSubmenuOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-logo">
@@ -18,7 +25,7 @@ const Navbar = () => {
         </Link>
       </div>
       <ul className="navbar-links">
-      <li><Link to="/">Home</Link></li>
+      <li><Link to="/">Inicio</Link></li>
         <li
           className="dropdown"
           onMouseEnter={handleDropdownToggle}
@@ -27,9 +34,26 @@ const Navbar = () => {
           <Link to="/competition">Competición</Link>
           {isDropdownOpen && (
             <ul className="dropdown-menu">
-              <li><Link to="/competition/season1">Season1</Link></li>
-              <li><Link to="/competition/season2">Season2</Link></li>
-              <li><Link to="/competition/rules">Reglamento</Link></li>
+              <li
+                className="submenu"
+                onMouseEnter={handleSubmenuToggle}
+                onMouseLeave={handleSubmenuToggle}
+              >
+                <Link to="/competition/season2">RCK Season 2</Link>
+                {isSubmenuOpen && (
+                  <ul className="submenu-menu">
+                    <li><Link to="/competition/season2/calendar">Calendario</Link></li>
+                    <li><Link to="/competition/season2/classification">Clasificación</Link></li>
+                  </ul>
+                )}
+              </li>
+              <li><Link to="/competition/overall-classification">Clasificación histórica</Link></li>
+              <li><Link to="/competition/hall-of-fame">Hall of fame</Link></li>
+              <li>
+                <a href="/reglamento.pdf" download="Reglamento_Rocketasso.pdf" target="_blank" rel="noopener noreferrer">
+                Reglamento
+                </a>
+              </li>
             </ul>
        )}
         </li>
